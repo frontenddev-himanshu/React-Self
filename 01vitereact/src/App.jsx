66e8b5fbef3fc5c16e755101190c99b17b99952f
frontenddev-himanshu <rajh5343@gmail.com>
2025-01-19 21:44:55 +0530

    add

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    <>
+    <Chai/>
+
+    <h1>
+      Himanshu
+    </h1>
+    </>
+  )
+}
+
+export default App
---
+++ customReact/customReact.js
@@ -0,0 +1,43 @@
+function customRender(reactElement, mainContainer) {
+    // const domElement = document.createElement(reactElement.type)
+
+    // domElement.innerHTML = reactElement.children
+
+    // domElement.setAttribute('href', reactElement.props.href)
+
+    // domElement.setAttribute('target', reactElement.props.target)
+
+    // mainContainer.appendChild(domElement)
+
+
+    const domElement = document.createElement(reactElement)
+
+    domElement.innerHTML = reactElement.children
+
+    for (const prop in reactElement.props) {
+
+        if (prop === children) continue;
+
+        domElement.setAttribute(prop, reactElement.props[prop])
+
+        container.appendChild(domElement)
+
+    }
+}
+
+const reactElement = {
+
+    type: "a",
+    props: {
+
+        herf: 'https://google.com',
+        target: '_blank'
+
+    },
+    children: 'click me to visit google'
+
+}
+
+const mainContainer = document.getElementById('root');
+
+customRender(reactElement, mainContainer)
